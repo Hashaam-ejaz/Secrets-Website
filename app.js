3731f0d1c7bc52290f97a5a3f3b63a4e0e8b22f1
Hashaam Ejaz <Hashaamejaz@gmail.com>
2023-04-19 11:56:11 +0500

    Username and Password Only

---
+++ app.js
@@ -0,0 +1,63 @@
+//jshint esversion:6
+const express = require("express");
+const bodyparser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser
+    .save()
+    .then((result) => {
+      res.render("secrets");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ email: username })
+    .then((result) => {
+      if (result) {
+        if (result.password === password) {
+          res.render("secrets");
+        }
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
